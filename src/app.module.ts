5800ad3460a9743f14443d528287b530d6f3bf4e
ngimdock <ngimdockzemfack@gmail.com>
2022-11-19 05:58:38 +0100

    Use forRootAsync to bootstrap the database connection after all modules are bootstraped

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,27 @@
-import { Inject, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import appConfig from './config/app.config';
 import * as Joi from '@hapi/joi';
 @Module({
   imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'postgres',
+        username: process.env.POSTGRES_USER,
+        password: process.env.POSTGRES_PASSWORD,
+        database: process.env.POSTGRES_NAME,
+        host: process.env.POSTGRES_HOST,
+        port: +process.env.POSTGRES_PORT,
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+    }),
+
     ConfigModule.forRoot({
       envFilePath: '.enviroment',
       validationSchema: Joi.object({
@@ -16,16 +29,6 @@ import * as Joi from '@hapi/joi';
         POSTGRES_PORT: Joi.number().default(5432),
       }),
       load: [appConfig],
-    }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      username: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_NAME,
-      host: process.env.POSTGRES_HOST,
-      port: +process.env.POSTGRES_PORT,
-      autoLoadEntities: true,
-      synchronize: true,
     }),
 
     CoffeesModule,
